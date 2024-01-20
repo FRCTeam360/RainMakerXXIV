ba0b7d4be1781cff346aeca94c833fcec9f8b01d
tbmilliren <145076363+Kaleb-Robotics@users.noreply.github.com>
2024-01-20 14:09:44 -0800

    created command RotateDriveTrain

---
+++ src/main/java/frc/robot/commands/RotateDriveTrain.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.PointWheelsAt;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+public class RotateDriveTrain extends Command {
+  private Rotation2d desiredAngle;
+  private CommandSwerveDrivetrain driveTrain;
+  
+  /** Creates a new RotateDriveTrain. */
+  public RotateDriveTrain(Rotation2d angle, CommandSwerveDrivetrain drive) {
+    driveTrain = drive;
+    desiredAngle = angle;
+    addRequirements(driveTrain);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    swerveRequest.withModuleDirection(desiredAngle);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
