99639ea0fae21fbae44240f1b75b0121e6290cba
juliashimshock <24shimshockjulia@bprep.org>
2024-01-26 14:53:37 -0800

    this code sucks

--- src/main/java/frc/robot/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/CommandSwerveDrivetrain.java
@@ -8,10 +8,14 @@ import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.subsystems.Limelight;
 
 /**
  * Class that extends the Phoenix SwerveDrivetrain class and implements subsystem
@@ -21,15 +25,21 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
     private static final double kSimLoopPeriod = 0.005; // 5 ms
     private Notifier m_simNotifier = null;
     private double m_lastSimTime;
+    private final Limelight lime;
+    private boolean useVision = true;
+    private final SwerveDriveOdometry odometry;
+    private final SwerveDrivePoseEstimator estimator;
 
-    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency, SwerveModuleConstants... modules) {
+    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency, Limelight lime, SwerveModuleConstants... modules) {
         super(driveTrainConstants, OdometryUpdateFrequency, modules);
+        this.lime = lime;
         if (Utils.isSimulation()) {
             startSimThread();
         }
     }
-    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
+    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, Limelight lime, SwerveModuleConstants... modules) {
         super(driveTrainConstants, modules);
+        this.lime = lime;
         if (Utils.isSimulation()) {
             startSimThread();
         }
@@ -37,6 +47,18 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
 
     public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
         return run(() -> this.setControl(requestSupplier.get()));
+    }
+
+    public Pose2d getPose(boolean visionAngle) {
+        synchronized (estimator) {
+            synchronized (odometry) {
+                return new Pose2d(
+                    estimator.getEstimatedPosition().getX(),
+                    estimator.getEstimatedPosition().getY(),
+                    visionAngle ? estimator.getEstimatedPosition().getRotation() : odometry.getPoseMeters().getRotation()
+                );
+            }
+        }
     }
 
     private void startSimThread() {
@@ -52,5 +74,10 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
             updateSimState(deltaTime, RobotController.getBatteryVoltage());
         });
         m_simNotifier.startPeriodic(kSimLoopPeriod);
+    }
+
+    @Override
+    public void periodic() {
+        
     }
 }
--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -4,29 +4,123 @@
 
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
-  NetworkTable lime = NetworkTableInstance.getDefault().getTable("limelight");
+  //NetworkTable lime = NetworkTableInstance.getDefault().getTable("limelight");
+  private static final double LOWEST_DISTANCE = Units.feetToMeters(10.0);
+  private final VisionIO[] cameras;
+  private final VisionIOInputsAutoLogged[] inputs;
+
+  private int acceptableID;
+  private boolean useSingleTag = false;
+
+  private final List<Limelight.PoseAndTimestamp> results = new ArrayList<>();
   /** Creates a new Limelight. */
-  public Limelight() {}
+  public Limelight(VisionIO[] cameras) {
+    this.cameras = cameras;
+    inputs = new VisionIOInputsAutoLogged[cameras.length];
 
-  public double getTX() {
-    return lime.getEntry("tx").getDouble(0);
+    for (int i = 0; i < cameras.length; i++) {
+        inputs[i] = new VisionIOInputsAutoLogged();
+    }
   }
 
-  public double getTY() {
-    return lime.getEntry("ty").getDouble(0);
-  }
+  // public double getTX() {
+  //   return lime.getEntry("tx").getDouble(0);
+  // }
 
-  public double getTV() {
-    return lime.getEntry("tv").getDouble(0);
-  }
+  // public double getTY() {
+  //   return lime.getEntry("ty").getDouble(0);
+  // }
+
+  // public double getTV() {
+  //   return lime.getEntry("tv").getDouble(0);
+  // }
 
   @Override
   public void periodic() {
+    Logger.recordOutput("useSingleTag", useSingleTag);
+    results.clear();
+    for(int i = 0; i < inputs.length; i++) {
+      cameras[i].updateInputs(inputs[i]);
+      Logger.processInputs("Vision/" + cameras[i].getName() + "/Inputs", inputs[i]);
+
+      if (inputs[i].hasTarget
+              && inputs[i].isNew
+              && !DriverStation.isAutonomous()
+              && inputs[i].maxDistance < LOWEST_DISTANCE) {
+          if (useSingleTag) {
+              if (inputs[i].singleIDUsed == acceptableID) {
+                  processVision(i);
+              }
+          } else {
+              processVision(i);
+          }
+      }
+    }
+    Logger.recordOutput("Vision/ResultCount", results.size());
     // This method will be called once per scheduler run
+  }
+
+  public List<Limelight.PoseAndTimestamp> getVisionOdometry() {
+    return results;
+  }
+
+  public static class PoseAndTimestamp {
+    Pose2d pose;
+    double timestamp;
+
+    public PoseAndTimestamp(Pose2d pose, double timestamp) {
+      this.pose = pose;
+      this.timestamp = timestamp;
+    }
+
+    public Pose2d getPose() {
+      return pose;
+    }
+
+    public double getTimestamp() {
+      return timestamp;
+    }
+  }
+
+  public void setUseSingleTag(boolean useSingleTag) {
+    setUseSingleTag(useSingleTag, 0);
+  }
+
+  public void setUseSingleTag(boolean useSingleTag, int accpetableID) {
+    this.useSingleTag = useSingleTag;
+    this.acceptableID = accpetableID;
+  }
+
+  public void setReferencePose(Pose2d pose) {
+    for(VisionIO io : cameras) {
+      io.setReferencePose(pose);
+    }
+  }
+
+  public double getMinDistance(int camera) {
+    return inputs[camera].minDistance;
+  }
+
+  public void processVision(int cameraNum) {
+    Pose2d currentPose =
+        new Pose2d(inputs[cameraNum].x, inputs[cameraNum].y, new Rotation2d(inputs[cameraNum].rotation));
+        Logger.recordOutput(cameras[cameraNum].getName() + " pose", currentPose);
+
+        // add the new pose to a list
+        results.add(new PoseAndTimestamp(currentPose, inputs[cameraNum].timestamp));
   }
 }
