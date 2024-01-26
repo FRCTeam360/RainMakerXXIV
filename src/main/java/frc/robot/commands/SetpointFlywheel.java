066fd931c5fcdd7946556b0e03ef4c22b225ad74
RuotT <25thiepruot@bprep.org>
2024-01-25 19:13:08 -0800

    Update SetpointFlywheel.java
    put code into setpointFlywheel(basic things like end, isFinished , and constructer code)

--- src/main/java/frc/robot/commands/SetpointFlywheel.java
+++ src/main/java/frc/robot/commands/SetpointFlywheel.java
@@ -4,66 +4,47 @@
 
 package frc.robot.commands;
 
-import com.revrobotics.SparkPIDController;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Shooter;
 
+
+
 public class SetpointFlywheel extends Command {
-  private double setpoint;
-  private Shooter shooter = new Shooter();
 
-  private double kP = 0.01;
-  private double kD = 0.0;
-  private double kI = 0.0;
-  private double kFF = 0.0;
-
-  public SetpointFlywheel(double setpointVelocity) {
-    setpoint = setpointVelocity;
+  private final Shooter shooter;
+  private double setpointRpm;
+  
+  /** Creates a new SetpointFlywheel. */
+  public SetpointFlywheel(Shooter shooter, double setpointRpm) {
+    this.shooter = shooter;
+    this.setpointRpm = setpointRpm;
+    addRequirements(this.shooter);
+    
+    // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    SmartDashboard.putNumber("p", kP);
-    SmartDashboard.putNumber("i", kI);
-    SmartDashboard.putNumber("d", kD);
-    SmartDashboard.putNumber("ff", kFF);
+    shooter.setSpeed(setpointRpm);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double p = SmartDashboard.getNumber("p", 0.01);
-    double i = SmartDashboard.getNumber("i", 0);
-    double d = SmartDashboard.getNumber("d", 0);
-    double ff = SmartDashboard.getNumber("ff", 0);
 
-    if ((p != kP)) {
-      shooter.leftPidController.setP(p);
-      shooter.rightPidController.setP(p);
-      kP = p;
-    }
-    if ((i != kI)) {
-      shooter.leftPidController.setI(i);
-      shooter.rightPidController.setI(i);
-      kI = i;
-    }
-    if ((d != kD)) {
-      shooter.leftPidController.setD(d);
-      shooter.rightPidController.setD(d);
-      kD = d;
-    }
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    shooter.stopLeft();
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    shooter.setSpeed(0.0);
     return false;
   }
 }
