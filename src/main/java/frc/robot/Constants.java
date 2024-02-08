92faba5a4080af6014f2f5d80f9c2bee0dcddceb
Reagan17aj <105814714+Reagan17aj@users.noreply.github.com>
2024-02-07 17:55:31 -0800

    Test MAC & Serial Address :)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,17 @@
 package frc.robot;
 
+import java.io.IOException;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import frc.robot.Constants.RobotType;
 
 public class Constants {
         public static final int DRIVER_CONTROLLER = 0;
@@ -13,19 +25,66 @@ public class Constants {
         public static final double MAX_SPEED_MPS = 13.7; // used to be 6 meters per second desired top speed (name changed from MAX_SPEED to MAX_SPEED_MPS jan25 kinda sketch)
         public static final double MAX_ANGULAR_RATE = Math.PI * 3; // Half a rotation per second max angular velocity
         
- public static final Mode currentMode = Mode.REAL;
- 
- public static enum Mode {
-    /** Running on a real robot. */
-    REAL,
+        // public static final Mode currentMode = Mode.REAL;
+        
+        // public static RobotType robot = RobotType.WOODBOT;
 
-    /** Running a physics simulator. */
-    SIM,
+		// toggle constants between comp bot and practice bot ("beta")
+        public static boolean isWoodBot;
+        public static boolean isPracticeBot;
+        public static boolean isCompBot;
+		public static boolean isTestBot;  
 
-    /** Replaying from a log file. */
-    REPLAY
-  }
-
-
-        public static final int PIGEON_ID = 13;
+        public static boolean isWoodBot(){
+                return isWoodBot;
+        }
+        public static boolean isPracticeBot(){
+                return isPracticeBot; 
+        }
+        public static boolean isCompBot(){
+                return isCompBot; 
+        }
+		public static boolean isTestBot(){
+			return isTestBot; 
+		}
+        
+ public static final class MacAddressConstants {
+	public static final byte[] TEST_ADDRESS = new byte[]{
+			// values are for test -> E4-42-A6-A0-82-05 
+			(byte) 0x00, (byte) 0x80, (byte) 0x2f, (byte) 0x28, (byte) 0x51, (byte) 0x35
+	};
 }
+public static final class SerialAddressConstants{
+        public static final String TEST_SERIAL_ADDRESS = "031b5208";
+}
+}
+
+
+
+//  public static enum RobotType {
+//     /* Running on a Wood Robot */
+//     WOODBOT, 
+
+//     /* Running on a Practice Robot */
+//     PRACTICE,
+
+//     /* Running on a Competition Robot */
+//     COMPETITION, 
+
+// 	/* Running on a Test Robot */
+// 	TEST
+//  }
+ 
+//  public static enum Mode {
+//     /** Running on a real robot. */
+//     REAL,
+
+//     /** Running a physics simulator. */
+//     SIM,
+
+//     /** Replaying from a log file. */
+//     REPLAY
+//   }
+
+
+    //public static final int PIGEON_ID = 13;
