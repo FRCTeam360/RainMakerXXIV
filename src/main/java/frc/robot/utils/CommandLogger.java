3ba47c3817722c819199c2eeb2a5b9785e768b3b
James Taylor <james.taylor2@comcast.net>
2024-02-09 13:59:03 -0800

    Added Javadoc comments to CommandLogger utility class

--- src/main/java/frc/robot/utils/CommandLogger.java
+++ src/main/java/frc/robot/utils/CommandLogger.java
@@ -8,20 +8,31 @@ import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-/** Add your docs here. */
+/**
+ * CommandLogger is a utility class that logs the start, running, and end of a command.
+ */
 public class CommandLogger {
-    // Log the command that is starting
+    /**
+     * Log the command that is starting
+     * @param command The command that is starting
+     */
     public static void logCommandStart(Command command) {
         Logger.recordOutput("Command Running: " + command.getClass().getName(), true);
         System.out.println("Print Command Start: " + command.getClass().getName());
     }
 
-    // Log the command that is running
+    /**
+     * Log the command that is running
+     * @param command The command that is running
+     */
     public static void logCommandRunning(Command command) {
         Logger.recordOutput("Command Running: " + command.getClass().getName(), true);
     }
 
-    // Log the command that is ending
+    /**
+     * Log the command that is ending
+     * @param command The command that is ending
+     */
     public static void logCommandEnd(Command command) {
         Logger.recordOutput("Command Running: " + command.getClass().getName(), false);
         System.out.println("Print Command End: " + command.getClass().getName());
