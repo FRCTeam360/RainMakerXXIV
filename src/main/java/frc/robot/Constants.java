b47cf04fd692bb529710835baf6c31a2409721e4
Reagan17aj <105814714+Reagan17aj@users.noreply.github.com>
2024-02-09 19:56:36 -0800

    removal of mac address constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,9 +17,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Constants {
         public static class WoodBotConstants {
-        
-                
+
         }
+
         public static final int DRIVER_CONTROLLER = 0;
         public static final int OPERATOR_CONTROLLER = 1;
 
@@ -28,101 +28,91 @@ public class Constants {
         public static final int SHOOTER_BOTTOM_ID = 3;
         public static final int INTAKE_ID = 5;
 
-        public static final double MAX_SPEED_MPS = 13.7; // used to be 6 meters per second desired top speed (name changed from MAX_SPEED to MAX_SPEED_MPS jan25 kinda sketch)
+        public static final double MAX_SPEED_MPS = 13.7; // used to be 6 meters per second desired top speed (name
+                                                         // changed from MAX_SPEED to MAX_SPEED_MPS jan25 kinda sketch)
         public static final double MAX_ANGULAR_RATE = Math.PI * 3; // Half a rotation per second max angular velocity
         public static final int PIGEON_ID = 13;
-        
+
         public static RobotType robot = getRobotType();
 
-	
-
-        public static boolean isWoodBot(){
-                if(getRobotType() == Constants.RobotType.WOODBOT){
+        public static boolean isWoodBot() {
+                if (getRobotType() == Constants.RobotType.WOODBOT) {
                         return true;
-                }else{
-                        return false; 
-                }
-        }
-        public static boolean isPracticeBot(){
-                if(getRobotType() == Constants.RobotType.PRACTICE){
-                        return true;
-                }else{
+                } else {
                         return false;
                 }
         }
-        public static boolean isCompBot(){
-                if(getRobotType() == Constants.RobotType.COMPETITION){
-                        return true; 
-                } else{
-                        return false; 
+
+        public static boolean isPracticeBot() {
+                if (getRobotType() == Constants.RobotType.PRACTICE) {
+                        return true;
+                } else {
+                        return false;
                 }
         }
-	public static boolean isTestBot(){
-		if(getRobotType() == Constants.RobotType.TEST){
-                        return true; 
-                } else{
-                        return false; 
+
+        public static boolean isCompBot() {
+                if (getRobotType() == Constants.RobotType.COMPETITION) {
+                        return true;
+                } else {
+                        return false;
                 }
-	}
-public static enum RobotType {
-    /** Running on a real robot. */
-    REAL,
-
-    /** Running a physics simulator. */
-    SIM,
-
-    /** Replaying from a log file. */
-    REPLAY, 
-
-    /* Running on a Wood Robot */
-    WOODBOT, 
-
-    /* Running on a Practice Robot */
-    PRACTICE,
-
-    /* Running on a Competition Robot */
-    COMPETITION, 
-
-    /* Running on a Test Robot */
-    TEST
- }
- 
-  public static RobotType getRobotType (){
-        String serialAddress = HALUtil.getSerialNumber();
-        SmartDashboard.putString("serial address", serialAddress);
-        SmartDashboard.putString("Test Serial Address", SerialAddressConstants.TEST_SERIAL_ADDRESS);
-
-        if(serialAddress.equals(SerialAddressConstants.TEST_SERIAL_ADDRESS)){
-                return Constants.RobotType.TEST;
-                
         }
-        else if(serialAddress.equals(SerialAddressConstants.WOOD_SERIAL_ADDRESS)){
-                return Constants.RobotType.WOODBOT; 
+
+        public static boolean isTestBot() {
+                if (getRobotType() == Constants.RobotType.TEST) {
+                        return true;
+                } else {
+                        return false;
+                }
         }
-        // else if(serialAddress.equals(SerialAddressConstants.PRACTICE_SERIAL_ADDRESS)){           
-        //         return Constants.RobotType.PRACTICE;
-        // }
-        else{
-                return Constants.RobotType.COMPETITION; 
 
+        public static enum RobotType {
+                /** Running on a real robot. */
+                REAL,
+
+                /** Running a physics simulator. */
+                SIM,
+
+                /** Replaying from a log file. */
+                REPLAY,
+
+                /* Running on a Wood Robot */
+                WOODBOT,
+
+                /* Running on a Practice Robot */
+                PRACTICE,
+
+                /* Running on a Competition Robot */
+                COMPETITION,
+
+                /* Running on a Test Robot */
+                TEST
         }
-  }
-        
- public static final class MacAddressConstants {
-	public static final byte[] TEST_ADDRESS = new byte[]{
-			// values are for test -> E4-42-A6-A0-82-05 
-			(byte) 0x00, (byte) 0x80, (byte) 0x2f, (byte) 0x28, (byte) 0x51, (byte) 0x35
-	};
+
+        public static RobotType getRobotType() {
+                String serialAddress = HALUtil.getSerialNumber();
+                SmartDashboard.putString("serial address", serialAddress);
+                SmartDashboard.putString("Test Serial Address", SerialAddressConstants.TEST_SERIAL_ADDRESS);
+
+                if (serialAddress.equals(SerialAddressConstants.TEST_SERIAL_ADDRESS)) {
+                        return Constants.RobotType.TEST;
+
+                } else if (serialAddress.equals(SerialAddressConstants.WOOD_SERIAL_ADDRESS)) {
+                        return Constants.RobotType.WOODBOT;
+                }
+                // else
+                // if(serialAddress.equals(SerialAddressConstants.PRACTICE_SERIAL_ADDRESS)){
+                // return Constants.RobotType.PRACTICE;
+                // }
+                else {
+                        return Constants.RobotType.COMPETITION;
+
+                }
+        }
+
+        public static final class SerialAddressConstants {
+                public static final String TEST_SERIAL_ADDRESS = "031b5208";
+                public static final String WOOD_SERIAL_ADDRESS = "03064db8";
+        }
 }
-public static final class SerialAddressConstants{
-        public static final String TEST_SERIAL_ADDRESS = "031b5208";
-        public static final String WOOD_SERIAL_ADDRESS = "03064db8";
-}
-}
-
-
-
-
-
-
-    
