57d925509af85a63045d29ace62648f4eb0357c2
Keirnan <142344414+27MahoneyKeirnan@users.noreply.github.com>
2024-03-19 17:31:17 -0700

    Updated SnapToAngle Angles

--- src/main/java/frc/robot/commands/SnapDrivebaseToAngle.java
+++ src/main/java/frc/robot/commands/SnapDrivebaseToAngle.java
@@ -10,8 +10,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class SnapDrivebaseToAngle extends Command {
-   private CommandSwerveDrivetrain swerveDrivetrain;
-  
+  private CommandSwerveDrivetrain swerveDrivetrain;
+
   /** Creates a new SnapDrivebaseToAngle. */
   public SnapDrivebaseToAngle(CommandSwerveDrivetrain swerveDrivetrain) {
     this.swerveDrivetrain = swerveDrivetrain;
@@ -26,42 +26,37 @@ public class SnapDrivebaseToAngle extends Command {
     if (DriverStation.getAlliance().get() == Alliance.Blue) {
       if (swerveDrivetrain.getAngle() >= 60 && swerveDrivetrain.getAngle() < 180) {
         angleToFace = 120.0;
-      }
-      else if (swerveDrivetrain.getAngle() >= 180 && swerveDrivetrain.getAngle() < 300) {
+      } else if (swerveDrivetrain.getAngle() >= 180 && swerveDrivetrain.getAngle() < 300) {
         angleToFace = 240.0;
-      }
-      else if(swerveDrivetrain.getAngle() >= 60 && swerveDrivetrain.getAngle() < 300) {
+      } else if (swerveDrivetrain.getAngle() < 60 && swerveDrivetrain.getAngle() > 300) {
         angleToFace = 0.0;
       }
 
       swerveDrivetrain.driveFieldCentricFacingAngle(0.0, 0.0, angleToFace);
 
-   } else if( DriverStation.getAlliance().get() == Alliance.Blue) {
-      if (swerveDrivetrain.getAngle() >= -60 && swerveDrivetrain.getAngle() < -180) {
-        angleToFace = -120;
-      }
-      else if (swerveDrivetrain.getAngle() >= -180 && swerveDrivetrain.getAngle() < -300) {
-        angleToFace = -240.0;
-      }
-      else if (swerveDrivetrain.getAngle() >= -300 && swerveDrivetrain.getAngle() < -60
-      ) {
-        angleToFace = 0.0;
+    } else if (DriverStation.getAlliance().get() == Alliance.Red) {
+      if (swerveDrivetrain.getAngle() >= 120 && swerveDrivetrain.getAngle() < 240) {
+        angleToFace = 180.0;
+      } else if (swerveDrivetrain.getAngle() >= 240 && swerveDrivetrain.getAngle() <= 0.0) {
+        angleToFace = 300.0;
+      } else if (swerveDrivetrain.getAngle() < 120.0 && swerveDrivetrain.getAngle() > 0.0) {
+        angleToFace = 60.0;
       }
       swerveDrivetrain.driveFieldCentricFacingAngle(0.0, 0.0, angleToFace);
 
-      
-   }
-}
+    }
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
