3042132dd58440487ed85fbeae89e433a302d073
RuotT <25thiepruot@bprep.org>
2024-03-30 16:00:06 -0700

    Cleaned up lights code

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix.led.CANdle;
@@ -29,39 +30,56 @@ import com.ctre.phoenix.led.LarsonAnimation.BounceMode;
 public class Lights extends SubsystemBase {
   /** Creates a new Lights. */
   private final CANdle lights = new CANdle(14);
+  private DigitalInput zeroButton;
+  private Linkage linkage;
+  private Vision vision;
   
-  public Lights() {
+  public Lights(DigitalInput zeroButton,Linkage linkage, Vision vision) {
     //lights.configLEDType(LEDStripType.GRB);
+    setFirstRed();
     lights.configBrightnessScalar(1);
-
+    this.zeroButton = zeroButton;
+    this.linkage = linkage;
+    this.vision = vision;
   }
-  public void isZero(DigitalInput zeroButton){
+  public void isZero(){
    if (!zeroButton.get()) {
-      lights.setLEDs(255, 0, 0,0,0,1);
-    } else if (zeroButton.get()) {
-      if (DriverStation.isDSAttached() && DriverStation.getAlliance().get() == Alliance.Blue) {
-        setBlue();
-      } else {
-        setOrange();
-      }
+      setFirstBlue();
+   }
+  }
+
+  public void linkageRange(){
+    if(linkage.getAngle() >= 120){
+      setFirstGreen();
     }
   }
-  // public void linkageRange(Linkage linkage){
-  //   if(linkage.getAngle() >= 120){
-  //     lights.setLEDs(4,212,132,0,0,7);
-  //   }
-  // }
-  public void setGreen(){
-    lights.setLEDs(4,212,132,0,0,7);
+
+  public void isTargetInView(){
+    if (vision.isTargetInView()) {
+      setAllOrange();
+    }
   }
-  public void setRed(){
-    lights.setLEDs(255, 0, 0,0,0,7);
+  
+  public void setFirstRed(){
+    lights.setLEDs(255, 0, 0,0,0,1);
   }
-  public void setBlue(){
-    lights.setLEDs(0,0,255,0,0,7);
+  public void setFirstGreen(){
+    lights.setLEDs(4,212,132,0,0,1);
   }
-  public void setOrange(){
-    lights.setLEDs(250,40,0,0,0,7);
+  public void setFirstBlue(){
+    lights.setLEDs(0,0,255,0,0,1);
+  }
+  public void setAllGreen(){
+    lights.setLEDs(4,212,132,0,8,20);
+  }
+  public void setAllRed(){
+    lights.setLEDs(255, 0, 0,0,8,20);
+  }
+  public void setAllBlue(){
+    lights.setLEDs(0,0,255,0,8,20);
+  }
+  public void setAllOrange(){
+    lights.setLEDs(250,40,0,0,8,20);
   }
   public void setAnimation(int r, int g, int b,String animationString){
     lights.clearAnimation(0);
@@ -83,6 +101,21 @@ public class Lights extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    if(RobotState.isDisabled()){
+     isZero();
+     linkageRange();
+     if (DriverStation.isDSAttached() && DriverStation.getAlliance().get() == Alliance.Blue) {
+      setAllBlue();
+    } else {
+      setAllOrange();
+    }
+    }else{
+      if(!RobotState.isAutonomous()){
+        isTargetInView();
+      }
+      
+      }
   }
 }
+
+ 
