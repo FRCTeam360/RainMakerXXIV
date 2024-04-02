52ad1c4caeee6225af24d7298480ac5b907e0263
Xtr3m3Boi5 <122415457+annashimshock@users.noreply.github.com>
2024-04-01 17:43:46 -0700

    path planning lol

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -236,26 +236,8 @@ public class RobotContainer {
   public RobotContainer() {
         field = new Field2d();
         SmartDashboard.putData("Field", field);
-
-        // Logging callback for current robot pose
-        PathPlannerLogging.setLogCurrentPoseCallback((pose) -> {
-            // Do whatever you want with the pose here
-            field.setRobotPose(pose);
-        });
-
-        // Logging callback for target robot pose
-        PathPlannerLogging.setLogTargetPoseCallback((pose) -> {
-            // Do whatever you want with the pose here
-            field.getObject("target pose").setPose(pose);
-            Logger.recordOutput("target pose", field.getObject("target pose").getPose());
-            Logger.recordOutput("path", (String[]) field.getObject("path").getPoses().toArray());
-        });
-
-        // Logging callback for the active path, this is sent as a list of poses
-        PathPlannerLogging.setLogActivePathCallback((poses) -> {
-            // Do whatever you want with the poses here
-            field.getObject("path").setPoses(poses);
-        });
+        Logger.recordOutput("target pose", field.getObject("target pose").getPose());
+        Logger.recordOutput("path", (String[]) field.getObject("path").getPoses().toArray());
 
     switch (Constants.getRobotType()) {
       case WOODBOT:
